629d2ba607c3574a6e0c81264e257762bdef6155
Ravinder sales <ravinder.y@crowdbotics.com>
2023-06-16 17:13:05 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,4 +1,4 @@
-import { api_v1_login_create } from "../../store/testingcbjunedevAPI/authTokens.slice.js";
+import { rest_auth_login_create } from "../../store/testingcbjunedevAPI/logins.slice.js";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import React from "react";
@@ -7,7 +7,9 @@ import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 const Untitled3 = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(api_v1_login_create());
+    dispatch(rest_auth_login_create({
+      test: "test123"
+    }));
   }, []);
   return <SafeAreaView style={styles.safeArea}>
       <ScrollView contentContainerStyle={{

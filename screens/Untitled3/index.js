66700111d4e3d5d41aeddf84aae4c05c7ebd6b1d
Ravinder sales <ravinder.y@crowdbotics.com>
2023-06-16 15:07:56 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,7 +1,14 @@
+import { api_v1_signup_create } from "../../store/testingcbjunedevAPI/signups.slice.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled3 = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(api_v1_signup_create());
+  }, []);
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
